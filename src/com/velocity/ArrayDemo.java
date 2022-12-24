b375788fca506e042f6dd1b95e4940e2c65a460a
gns-naresh <gaikwadnaresh736@gmail.com>
2022-12-24 20:13:00 +0530

    first commt : code

---
+++ src/com/velocity/ArrayDemo.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> arraylist = new ArrayList<String>();
+		arraylist.add("naresh");
+		arraylist.add("nikhil");
+		arraylist.add("sarvankika");
+		arraylist.add("uday");
+
+		System.out.println(arraylist);
+
+	}
+
+}
